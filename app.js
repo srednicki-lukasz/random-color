8c190732e299c2975279f67ecebd6f02910ad01f
Lukasz <srednickilukasz@gmail.com>
2021-05-05 23:04:45 +0200

    Copy to clipboard

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ let hex = '#000000';
 let colorCode = document.querySelector('.color-code');
 
 const preview = document.querySelector('.preview');
-const btnCopyCss = document.querySelector('.show-css');
+const btnCopyCss = document.querySelector('.copy-css');
 const btnCopyHex = document.querySelector('.copy-hex');
 
+// change background color
 preview.addEventListener('click', () => {
     hex = '#';
     const chars = ['A', 'B', 'C', 'D', 'E', 'F', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
@@ -15,4 +16,24 @@ preview.addEventListener('click', () => {
 
     colorCode.value = hex;
     preview.style.background = hex;
+});
+
+// copy to clipboard
+btnCopyCss.addEventListener('click', () => {
+    let input = document.createElement('input');
+    input.value = `background-color: ${hex}`;
+    document.body.appendChild(input);
+    input.select();
+    input.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    document.body.removeChild(input);
+    alert('CSS code copied!');
+});
+
+btnCopyHex.addEventListener('click', () => {
+    colorCode.select();
+    colorCode.setSelectionRange(0, 99999);
+    document.execCommand('copy');
+    colorCode.setSelectionRange(0, 0)
+    alert('Hex code copied!');
 });
